1aa661f6777297498cc31905be94b1e32812cd33
abhi-ghute <abghute7@gmail.com>
2022-11-26 20:23:07 +0530

    spring security app

---
+++ src/main/java/com/security/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/list")
+	public String getUserList() {
+		return "userlist";
+	}
+}
